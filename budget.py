9625ec680f0da6a17d44d2aed3d53ab8414a4f8f
Jusepez11 <44486060+Jusepez11@users.noreply.github.com>
2022-08-01 20:53:12 -0400

    Creating budget.py
    Also creating the main functions for said class

---
+++ budget.py
@@ -0,0 +1,24 @@
+class Category:
+
+  
+
+
+
+
+  def create_spend_chart(categories):
+    pass 
+
+  def deposit():
+    pass
+
+  def withdraw():
+    pass
+
+  def get_balance():
+    pass
+
+  def transfer():
+    pass
+  
+  def check_funds():
+    pass
