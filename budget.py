4322a23a55ed90403a3caabbcae716954b30742e
Jusepez11 <44486060+Jusepez11@users.noreply.github.com>
2022-08-01 21:42:10 -0400

    modified withdraw
    checks the current balance and adds it to the ledger and returns true if it is available or returns false

--- budget.py
+++ budget.py
@@ -9,8 +9,19 @@ class Category:
   def deposit(self, amount, description=""):
     self.ledger.append({"amount": amount, "description": description})
 
-  def withdraw(self):
-    pass
+  def withdraw(self, amount):
+    balance = 0
+
+    #Loping around the ledger to add all the transactions
+    for i in range(len(self.ledger)):
+        balance += self.ledger[i]["amount"]
+    
+    #Checks if the balance is equal or higher than the amount that will be withdrawn
+    if balance >= amount:
+        self.ledger.append({"amount": amount * -1})
+        return True
+    
+    return False
 
   def get_balance(self):
     pass
