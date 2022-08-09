8bbf720e04c4413b6fcb1c14ac027545e82d786f
Jusepez11 <44486060+Jusepez11@users.noreply.github.com>
2022-08-09 16:52:37 -0400

    Updating transfer and more
    added description to withdraw and temporarily modified __init__ so that the fist line of main.py works, I'll figure it out as i go, what is it that i need to do there

--- budget.py
+++ budget.py
@@ -1,18 +1,20 @@
 class Category:
 
-  def __init__(self):
+  def __init__(self, name):
+    self.name = name
     self.ledger = []
 
   def create_spend_chart(self, categories):
     pass 
 
   def deposit(self, amount, description=""):
+    #adding a deposit to the ledger
     self.ledger.append({"amount": amount, "description": description})
 
-  def withdraw(self, amount):    
+  def withdraw(self, amount, description=""):    
     #Checks if the balance is equal or higher than the amount that will be withdrawn
     if check_funds(amount):
-        self.ledger.append({"amount": amount * -1})
+        self.ledger.append({"amount": amount * -1, "description": description})
         return True
     return False
 
@@ -24,11 +26,18 @@ class Category:
 
     return balance
 
-  def transfer(self):
-    pass
+  def transfer(self, amount, bCategory):
+    #if there are funds the transfer will be made and the function will return true, otherwise it will return false
+    if check_funds(amount):
+      withdraw(amount, ("Transfer to %s" % bCategory))
+      deposit(amount, ("Transfer from %s" % bCategory))
+      return True
+    return False
   
   def check_funds(self, amount):
     if get_balance() >= amount:
         return True
     
     return False
+
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import budget
-from budget import create_spend_chart
+#from budget import create_spend_chart
 from unittest import main
 
 food = budget.Category("Food")
